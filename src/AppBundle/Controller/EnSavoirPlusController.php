70a8ffddb418d439c9fe6a3c15f6b780ae1a53f8
Morgan Merzouk <morgan.merzouk@gmail.com>
2015-03-06 20:32:37 +0100

    Initialisation du front En Savoir Plus

--- src/AppBundle/Controller/EnSavoirPlusController.php
+++ src/AppBundle/Controller/EnSavoirPlusController.php
@@ -12,6 +12,30 @@ class EnSavoirPlusController extends Controller
      */
     public function indexAction()
     {        
-        return $this->render('AppBundle:Front:ensavoirplus.html.twig');
+        return $this->render('AppBundle:Front:ensavoirplus/ensavoirplus.html.twig');
+    }
+    
+    /**
+     * @Route("/ensavoirplus/croisiere", name="ensavoirplus_croisiere")
+     */
+    public function croisiereAction()
+    {
+        return $this->render('AppBundle:Front:ensavoirplus/ensavoirplus_croisiere.html.twig');
+    }
+    
+    /**
+     * @Route("/ensavoirplus/reseau", name="ensavoirplus_reseau")
+     */
+    public function reseauAction()
+    {
+        return $this->render('AppBundle:Front:ensavoirplus/ensavoirplus_reseau.html.twig');
+    }
+
+    /**
+     * @Route("/ensavoirplus/meteo", name="ensavoirplus_meteo")
+     */
+    public function meteoAction()
+    {
+        return $this->render('AppBundle:Front:ensavoirplus/ensavoirplus_meteo.html.twig');
     }
 }
