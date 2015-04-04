e57a6aa066e7f56c151df4e64dc03e76d4704b36
Morgan Merzouk <morgan.merzouk@gmail.com>
2015-04-04 12:31:30 +0200

    Amélioration des pages prestations

--- src/AppBundle/Controller/PrestationController.php
+++ src/AppBundle/Controller/PrestationController.php
@@ -3,6 +3,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\Query;
 
 class PrestationController extends Controller
 {
@@ -62,9 +63,45 @@ class PrestationController extends Controller
             ->getQuery()
             ->getResult();
         
+        $bateaux = $this->getDoctrine()
+            ->getManager()
+            ->getRepository("AppBundle\Entity\Bateau")
+            ->createQueryBuilder('b')
+            ->select('b, ipe, ipa, ipf, ipet, ipac, ipc, ipas')
+            ->leftJoin('b.inclusPrixEquipage', 'ipe')
+            ->leftJoin('b.inclusPrixAvitaillement', 'ipa')
+            ->leftJoin('b.inclusPrixFraisVoyage', 'ipf')
+            ->leftJoin('b.inclusPrixEquipement', 'ipet')
+            ->leftJoin('b.inclusPrixActivite', 'ipac')
+            ->leftJoin('b.inclusPrixCours', 'ipc')
+            ->leftJoin('b.inclusPrixAutresServices', 'ipas')
+            ->getQuery()
+            ->getResult(Query::HYDRATE_OBJECT);
+        
+        $ids = array();
+        
+        $boats = array();
+        foreach ($bateaux as $bateau) {
+            if ($bateau->getInclusPrixEquipage() != null && count($bateau->getInclusPrixEquipage()->getPrestation()) > 0) {
+                foreach ($bateau->getInclusPrixEquipage()->getPrestation() as $laprestation) {
+                    if ($laprestation->getId() == $id)
+                        array_push($boats, $bateau);
+                }
+            }
+            $inclusPrix = array_merge($bateau->getInclusPrixAvitaillement()->toArray(), $bateau->getInclusPrixFraisVoyage()->toArray(), $bateau->getInclusPrixEquipement()->toArray(), $bateau->getInclusPrixActivite()->toArray(), $bateau->getInclusPrixCours()->toArray(), $bateau->getInclusPrixAutresServices()->toArray());
+            foreach ($inclusPrix as $unInclusPrix) {
+                foreach ($unInclusPrix->getPrestation() as $uneprestation) {
+                    if ($uneprestation->getId() == $id) {
+                        array_push($boats, $bateau);
+                    }
+                }
+            }
+        }
+        
         return $this->render('AppBundle:Front:prestation.html.twig', array(
             'prestationsName' => $prestationsName,
-            'prestation' => $prestation
+            'prestation' => $prestation,
+            'boats' => $boats
         ));
     }
 }
