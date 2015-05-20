9aae13e5372ac97ec887231881f67b6bcec9e9b7
Morgan Merzouk <morgan.merzouk@gmail.com>
2015-05-20 09:57:04 +0200

    Ajout analytics, gestion i18n multidomaine en prod

---
+++ src/AppBundle/Listener/LocaleListener.php
@@ -0,0 +1,60 @@
+<?php
+namespace AppBundle\Listener;
+
+use \Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use \Symfony\Component\HttpKernel\KernelEvents;
+use \Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+
+class LocaleListener implements EventSubscriberInterface
+{
+
+    protected $domainLocales;
+
+    protected $defaultLocale;
+
+    public function __construct($container, $defaultLocale)
+    {
+        $this->domainLocales = $container->getParameter('domain_locales');
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    /**
+     * Set default locale
+     *
+     * @param GetResponseEvent $event            
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (! $request->hasPreviousSession()) {
+            return;
+        }
+        // get domain name
+        $host = $request->getHttpHost();
+        // or $host = $request->getHost();
+        
+        $locale = $this->defaultLocale;
+        
+        if (array_key_exists($host, $this->domainLocales)) {
+            $locale = $this->domainLocales[$host];
+        }
+        $request->setLocale($locale);
+    }
+
+    /**
+     * @ERROR!!!
+     */
+    static public function getSubscribedEvents()
+    {
+        return array(
+            // must be registered before the default Locale listener
+            KernelEvents::REQUEST => array(
+                array(
+                    'onKernelRequest',
+                    17
+                )
+            )
+        );
+    }
+}
