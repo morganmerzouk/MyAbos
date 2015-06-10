3c6b656eea91894804b0a66c928c2e52634c9e08
Morgan Merzouk <morgan.merzouk@gmail.com>
2015-06-11 00:08:11 +0200

    Amélioration de la home (video youtube et explication)

--- src/AppBundle/Resources/public/js/home.js
+++ src/AppBundle/Resources/public/js/home.js
@@ -14,3 +14,31 @@ $(document).ready(function(){
 		this.play();
 	});
 });
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '300',
+    width: '100%',
+    videoId: 'y_C6i1IBVE4',
+    playerVars: { 'autoplay': 1, 'controls': 0, 'html5': 1 },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+  event.target.mute();
+}
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING ) {}
+}
